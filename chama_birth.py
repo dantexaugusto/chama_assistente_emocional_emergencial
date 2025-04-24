481f36db6433e63af40443ab8606e5dbe3c9ce0e
dantex <dantexaugusto@gmail.com>
2025-04-24 17:28:01 -0300

    unfinished changes

--- chama_birth.py
+++ chama_birth.py
@@ -1,28 +1,102 @@
 from dotenv import load_dotenv
 import os
 from openai import OpenAI
-load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+import json
 
-with open("behaviour_prompt.txt", "r") as bPrompt:
-    behaviour_prompt = bPrompt.read()
+def model_response(conversation_history):
 
-user_prompt = input("Digite o prompt: \n",)
+    load_dotenv()
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+   
+    response = client.responses.create(
+        model="gpt-4.1-nano",
+        input= conversation_history
+    )
 
-response = client.responses.create(
-    model="gpt-4.1",
-    input=[
+    return response.output_text
+
+def user_history_manager(usrID, usrMessage):
+
+    with open("behaviour_prompt_I.txt", "r") as bPrompt:
+        behaviour_prompt = bPrompt.read()
+
+    conversation_start = [
         {
             "role": "developer",
             "content": behaviour_prompt
         },
         {
             "role": "user",
-            "content": user_prompt
+            "content": "se apresente, diga quem é você e o que você faz."
         }
     ]
-)
 
-print(response.output_text)
 
+    try:
+        with open("usrids.json", "r", encoding="utf-8") as idsJson:
+            usrIDs_convDict = json.load(idsJson)
+            
+            if usrID in usrIDs_convDict.keys():
+                conversation_history = usrIDs_convDict[usrID]
+    
+                return conversation_history
+            else:
+                
+    except Exception as e:
+        print("Sem arquivos para históricos de conversas", "\n", f"{e}")
+
+        usrIDs_convDict = {usrID:conversation_start}
+
+        with open("usrids.json", "w", encoding="utf-8") as idsjson:
+            json.dump(usrIDs_convDict, idsjson, indent=4, ensure_ascii=False)
+
+
+
+def main():
+
+    with open("behaviour_prompt_I.txt", "r") as bPrompt:
+        behaviour_prompt = bPrompt.read()
+
+    conversation_history = [
+        {
+            "role": "developer",
+            "content": behaviour_prompt
+        },
+        {
+            "role": "user",
+            "content": "se apresente, diga quem é você e o que você faz."
+        }
+    ]
+
+    print("Este é um loop infinito de conversação, para sair digite: quit", "\n")
+
+    assistant_welcoming = model_response(conversation_history)
+
+    conversation_history.append({"role":"assistant", "content": assistant_welcoming})
+
+    print(f"{conversation_history[2]["content"]}","\n")
+
+    while True:
+
+        most_recent_user_prompt = input("User: ")
+
+        if most_recent_user_prompt == "quit":
+            
+            with open("conversation_histoy.json", "w", encoding="utf-8") as hFile:
+                json.dump(conversation_history, hFile, indent=4, ensure_ascii=False) 
+
+            break
+
+        most_recent_user_state = {"role": "user", "content": most_recent_user_prompt}
+        
+        conversation_history.append(most_recent_user_state)
+        
+        most_recent_model_state = {"role": "assistant", "content": model_response(conversation_history)}
+
+        conversation_history.append(most_recent_model_state)
+        print("\n")
+        print(f"Assistant: {most_recent_model_state["content"]}", "\n")
+
+if __name__ == "__main__":
+    main()
 
